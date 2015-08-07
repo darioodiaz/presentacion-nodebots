ed457e17b3d408e12d57ac4b599de3369fa7ec9c
darioo.diaz1 <darioo.diaz1@gmail.com>
2015-08-07 19:46:59 -0300

    advance

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-console.log("Server started 8000");
-connect().use(serveStatic(__dirname)).listen(8000);
+console.log("Server started 9000");
+connect().use(serveStatic(__dirname)).listen(9000);
